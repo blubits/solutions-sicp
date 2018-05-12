9acb4675944df579f50f024ddc4c13d85d7595b5
Maded Batara III <mnbatara3@gmail.com>
2018-05-13 00:41:28 +0800

    solve: 1.12

---
+++ 01/1.12.rkt
@@ -0,0 +1,24 @@
+#lang sicp
+
+; Exercise 1.12
+; The following paern of numbers is called Pascal’s triangle.
+;     1
+;    1 1
+;   1 2 1
+;  1 3 3 1
+; 1 4 6 4 1
+;    ...
+; The numbers at the edge of the triangle are all 1, and each
+; number inside the triangle is the sum of the two numbers
+; above it. Write a procedure that computes elements of
+; Pascal’s triangle by means of a recursive process.
+
+(define (C x y)
+  (cond ((= x 0) 1)
+        ((= y 0) 1)
+        ((= x y) 1)
+        (else (+ (C (dec x) y)
+                 (C (dec x) (dec y))))))
+
+; should output 10
+(C 5 3)
