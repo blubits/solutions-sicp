f1f03288853d4d79b83a928981713c1dfacb95ed
Maded Batara III <mnbatara3@gmail.com>
2018-05-12 18:13:25 +0800

    solve: 1.08

---
+++ 01/1.08.rkt
@@ -0,0 +1,35 @@
+#lang sicp
+
+; Exercise 1.8
+; Newton’s method for cube roots is based on the fact that if y is an
+; approximation to the cube root of x, then a better approximation is given by
+; the value (x/y^2 + 2y)/3.
+; Use this formula to implement a cube-root procedure analogous to the
+; square-root procedure. (In 1.3.4 we will see how to implement Newton’s
+; method in general as an abstraction of these square-root and cube-root
+; procedures.)
+
+(define (square x) (* x x))
+
+(define (curt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (curt-iter (improve guess x) x)))
+
+(define (f x y)
+  (/ (+ (/ x (square y)) (* 2 y)) 3))
+
+(define (improve guess x)
+  (f x guess))
+
+(define (good-enough? guess x)
+  (< (abs (- (improve guess x) guess))
+     (* guess 0.0001)))
+
+(define (curt x)
+  (curt-iter 1.0 x))
+
+(curt 8)
+(curt 24)
+(curt 0.001)
+(curt 1000000000000000)
