f5840f623e7c1cdcc72a96fa41fdbf1363018b39
Maded Batara III <mnbatara3@gmail.com>
2018-05-15 00:49:59 +0800

    solve: 1.30

---
+++ 01/1.30.rkt
@@ -0,0 +1,31 @@
+#lang sicp
+
+; Exercise 1.30
+; The sum procedure above generates a linear recursion. The procedure can be
+; rewritten so that the sum is performed iteratively. Show how to do this by
+; filling in the missing expressions in the following definition:
+
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (+ result (term a)))))
+  (iter a 0))
+
+; should output 55
+(define (identity x) x)
+
+(define (sum-integers a b)
+  (sum identity a inc b))
+
+(sum-integers 1 10)
+
+; should output approx pi
+(define (pi-sum a b)
+  (define (pi-term x)
+    (/ 1.0 (* x (+ x 2))))
+  (define (pi-next x)
+    (+ x 4))
+  (sum pi-term a pi-next b))
+
+(* 8 (pi-sum 1 1000))
